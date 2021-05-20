69648dafa145d127f0b91a8132cdb2686d2b631c
Ricardo Pereira <ricardo.pereira@weareforeteller.com>
2021-05-21 00:51:35 +0100

    remove cache

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,6 @@
 import CMS from 'netlify-cms-app'
-
-
 import IndexPreview from './preview-templates/IndexPreview'
 
-CMS.registerPreviewStyle({styles});
 
+CMS.registerPreviewTemplate('index', IndexPreview)
 
