3ad168b68966221414972f03a95c0fa5fa532b30
Ricardo Pereira <ricardo.pereira@weareforeteller.com>
2021-05-20 23:49:47 +0100

    change styles - testing

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,8 @@
 import CMS from 'netlify-cms-app'
-
+import styles from './src/styles/global.css'
 
 import IndexPreview from './preview-templates/IndexPreview'
 
-CMS.registerPreviewStyle("./src/styles/global.css");
+CMS.registerPreviewStyle({styles});
 CMS.registerPreviewTemplate('index', IndexPreview)
 
