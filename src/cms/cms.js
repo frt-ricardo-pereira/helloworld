0b37feadb2ac588fcce22f1ed2f162d8bcc14c38
Ricardo Pereira <ricardo.pereira@weareforeteller.com>
2021-05-21 00:01:48 +0100

    remove style

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,8 @@
 import CMS from 'netlify-cms-app'
-import styles from '../styles/global.css'
+
 
 import IndexPreview from './preview-templates/IndexPreview'
 
 CMS.registerPreviewStyle({styles});
-CMS.registerPreviewTemplate('index', IndexPreview)
+
 
